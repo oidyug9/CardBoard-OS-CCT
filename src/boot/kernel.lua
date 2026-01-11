1c578d1111f6b2b7629a513babbfe08838137978
bluescreen <olivierus303@gmail.com>
2026-01-11 15:39:11 +0000

    craft os globals request idea

--- src/boot/kernel.lua
+++ src/boot/kernel.lua
@@ -1,5 +1,43 @@
--- data
+-- info
+KernelInfo = {}
+KernelInfo.version = 0.01
+
+-- kernel
+Kernel = {}
+Kernel.Terminals = {} -- multi terminal
+
+
+
+
+
+-- custom enveriouvment
 KernelData = {}
-KernelData.version = 0.01
+KernelData.AdminPassword = '123456' -- to do: add safe way to store password ( lock it behind custom fs so nobody can read it)
+KernelData.Files = {}
 
+KernelData.CustomGlobals = {}
+KernelData.CustomGlobals.NotCraftOsGlobals = true
 
+function KernelData.CustomGlobals.__request_craftos_globals__() -- return globals with admin perms
+    local tries = 5
+    for i=1, tries do -- try 5 times
+        
+        print("please enter ")
+        local password = read("*")
+
+        if password == KernelData.AdminPassword then
+            return _G -- return globals
+        else
+            print("wrong password, try again. tries left: "..(tries-i)+1)
+        end
+    end
+    return nil
+end
+
+function CreateVirtualImageOfDir(dir)
+    local VirtualImage = {}
+end
+
+function CustomFileSystem()
+    
+end
