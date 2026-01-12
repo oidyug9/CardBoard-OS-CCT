789913918461c4783b679685f7e836400fa27fc7
Bluescreen-yt <olivierus303@gmail.com>
2026-01-12 02:07:17 +0100

    event and process object (to do: improve process object and move it to objects)

--- src/boot/kernel.lua
+++ src/boot/kernel.lua
@@ -4,10 +4,28 @@ KernelInfo.version = 0.01
 
 -- kernel
 Kernel = {}
-Kernel.Terminals = {} -- multi terminal
+Kernel.Processes = {} -- processes
 
+process = {}
+process.__index = process
 
+function process.new(func)
+  local self = setmetatable({}, process)
 
+  self.func = func
+  self.id = #Kernel.Processes
+  table.insert(Kernel.Processes, self)
+  
+  return self
+end
+
+function process:run(...)
+  self.func(self, ...)
+end
+
+function process:kill()
+  table.remove(Kernel.Processes, self)
+end
 
 
 -- custom enveriouvment
@@ -15,10 +33,10 @@ KernelData = {}
 KernelData.AdminPassword = '123456' -- to do: add safe way to store password ( lock it behind custom fs so nobody can read it)
 KernelData.Files = {}
 
-KernelData.CustomGlobals = {}
-KernelData.CustomGlobals.NotCraftOsGlobals = true
+KernelData.CBGlobals = {}
+KernelData.CBGlobals.NotCraftOsGlobals = true
 
-function KernelData.CustomGlobals.__request_craftos_globals__() -- return globals with admin perms
+function KernelData.CBGlobals.__request_craftos_globals__() -- return globals with admin perms
     local tries = 5
     for i=1, tries do -- try 5 times
         
