8fd9bbb77ea0f07b06ffadc8b0b98de4d7bbf9f3
Bluescreen-yt <olivierus303@gmail.com>
2026-01-12 08:54:07 +0100

    to do: code the objects in /boot/objects

---
+++ src/boot/globals/KernelApi.lua
@@ -0,0 +1,20 @@
+--------------------------------
+--
+--           setup
+--
+--------------------------------
+
+local objectsPath = "../objects/"
+local CBglobals = {}
+
+
+for _, obj in fs.list(objectsPath) do 
+    local file = string.sub(obj, 1, -5)
+    
+
+
+end
+
+
+
+
