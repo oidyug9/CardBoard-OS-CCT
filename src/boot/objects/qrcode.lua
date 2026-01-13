498647ff4e80d4fb94319ef55fec7d3b7e05063b
bluescreen <olivierus303@gmail.com>
2026-01-13 15:57:38 +0000

    qr to do: add paint function and get info (with height etc)

--- src/boot/objects/qrcode.lua
+++ src/boot/objects/qrcode.lua
@@ -1,12 +1,38 @@
+
 local QrCode = {}
+
+QrCode.AsciiStartOffset = 32
+QrCode.AsciiEnd = 126
+QrCode.Ascii = {}
+
+for i = QrCode.AsciiStartOffset, QrCode.AsciiEnd do
+    QrCode.Ascii[string.char(i)] = i
+end
+
 QrCode.__index = QrCode
 
 function QrCode.new()
     return setmetatable({} ,QrCode)
 end
 
+
+function ToBinary(num)
+    local bin = ""
+    for i = 7, 0, -1 do
+        bin = bin .. ((num >> i) & 1)
+    end
+    return bin
+end
+
 function QrCode:generate(data)
-    
+    self.data = data
+    self.QrData = ""
+    self.QrHeader = ToBinary(#data)
+    for character in string.gmatch(data, '.') do
+        self.QrData = self.QrData .. ToBinary(QrCode.Ascii[character])
+    end
+
+    return self.QrData
 end
 
 function QrCode:getInfo()
@@ -16,3 +42,7 @@ end
 function QrCode:paint(x, y)
     
 end
+
+
+
+return QrCode
