710d11bca67ff4d07eef7cfa471f64363c10edce
bluescreen <olivierus303@gmail.com>
2026-01-13 15:03:11 +0000

    qr object {structure} (to do)

---
+++ src/boot/objects/qrcode.lua
@@ -0,0 +1,18 @@
+local QrCode = {}
+QrCode.__index = QrCode
+
+function QrCode.new()
+    return setmetatable({} ,QrCode)
+end
+
+function QrCode:generate(data)
+    
+end
+
+function QrCode:getInfo()
+    
+end
+
+function QrCode:paint(x, y)
+    
+end
